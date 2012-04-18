832224e04b440a54815019dab604e78684d35d7e
Daniel Ampuero <danielmaxx@gmail.com>
2012-04-18 04:46:38 +0000

    Se separa la aplicación bridgeinspections del proyecto Sismocaracas
    --
    danielmaxx

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+    
+'''
+IMPORTANTE: los campos que se describen a continuaciÃ³n deben ser rellenados en su totalidad.
+Aquellos campos que NO SE VAYAN A UTILIZAR, se deben BORRAR.
+'''
+
+
+setup(name='fvisbridgeinspections',
+      version='0.1',
+      author='Daniel Ampuero',
+      author_email='danielmaxx@gmaiÃl.com',
+#      url='',
+#      download_url='',
+      description='An application for store and process bridges',
+#      long_description='',
+      package_dir={'':'src'},
+      packages=['funvisis', 'funvisis.bridgeinspections'],
+#      py_modules=[''],
+#      provides=[''],
+#      keywords='',
+#      license='',
+      classifiers=['Development Status :: Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6+',
+                   'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                   'License :: OSI Approved :: GNU Affero General Public License v3',
+                   'Topic :: Internet',
+                   'Topic :: Scientific/Engineering :: GIS',
+                  ],
+       requires=['Django (>= 1.3)'],
+     )
